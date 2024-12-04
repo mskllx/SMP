af444d3a9a728154683ddd78b2cb6efc1340c2a7
Mykhailo Sikaliuk <mishasikalyuk1@gmail.com>
2024-12-05 00:05:24 +0300

    Added number, boolean, string

--- index.js
+++ index.js
@@ -1,224 +1,224 @@
-function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function generateRandomNumber(minNumber, maxNumber) {
+    return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 }
 
-function createArray(length, valueGenerator) {
-    var arr = [];
-    for (var i = 0; i < length; i++) {
-        arr.push(valueGenerator());
+function createArrayNumber(lengthNumber, valueGeneratorFunction) {
+    var arrayNumber = [];
+    for (var indexNumber = 0; indexNumber < lengthNumber; indexNumber++) {
+        arrayNumber.push(valueGeneratorFunction());
     }
-    return arr;
+    return arrayNumber;
 }
 
-var randomNumbers = createArray(100, function() {
+var randomNumbersArray = createArrayNumber(100, function() {
     return generateRandomNumber(1, 100);
 });
 
-function sumArray(numbers) {
-    var sum = 0;
-    for (var i = 0; i < numbers.length; i++) {
-        sum += numbers[i];
+function sumArrayNumbers(numbersArray) {
+    var sumNumber = 0;
+    for (var indexNumber = 0; indexNumber < numbersArray.length; indexNumber++) {
+        sumNumber += numbersArray[indexNumber];
     }
-    return sum;
+    return sumNumber;
 }
 
-var total = sumArray(randomNumbers);
+var totalSumNumber = sumArrayNumbers(randomNumbersArray);
 
-console.log("Сума випадкових чисел: " + total);
+console.log("Сума випадкових чисел: " + totalSumNumber);
 
-function bubbleSort(arr) {
-    var n = arr.length;
-    for (var i = 0; i < n - 1; i++) {
-        for (var j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                var temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+function bubbleSortArrayNumber(arrNumber) {
+    var nNumber = arrNumber.length;
+    for (var iNumber = 0; iNumber < nNumber - 1; iNumber++) {
+        for (var jNumber = 0; jNumber < nNumber - iNumber - 1; jNumber++) {
+            if (arrNumber[jNumber] > arrNumber[jNumber + 1]) {
+                var tempNumber = arrNumber[jNumber];
+                arrNumber[jNumber] = arrNumber[jNumber + 1];
+                arrNumber[jNumber + 1] = tempNumber;
             }
         }
     }
-    return arr;
+    return arrNumber;
 }
 
-var sortedNumbers = bubbleSort(randomNumbers.slice());
+var sortedNumbersArray = bubbleSortArrayNumber(randomNumbersArray.slice());
 
-console.log("Відсортовані числа: " + sortedNumbers.join(", "));
+console.log("Відсортовані числа: " + sortedNumbersArray.join(", "));
 
-function factorial(n) {
-    if (n === 0 || n === 1) {
+function factorialNumber(nNumber) {
+    if (nNumber === 0 || nNumber === 1) {
         return 1;
     } else {
-        return n * factorial(n - 1);
+        return nNumber * factorialNumber(nNumber - 1);
     }
 }
 
-var factOfTen = factorial(10);
+var factOfTenNumber = factorialNumber(10);
 
-console.log("Факторіал числа 10: " + factOfTen);
+console.log("Факторіал числа 10: " + factOfTenNumber);
 
-function isPrime(num) {
-    if (num <= 1) return false;
-    if (num <= 3) return true;
-    if (num % 2 === 0 || num % 3 === 0) return false;
-    for (var i = 5; i * i <= num; i += 6) {
-        if (num % i === 0 || num % (i + 2) === 0) return false;
+function isPrimeBoolean(numNumber) {
+    if (numNumber <= 1) return false;
+    if (numNumber <= 3) return true;
+    if (numNumber % 2 === 0 || numNumber % 3 === 0) return false;
+    for (var iNumber = 5; iNumber * iNumber <= numNumber; iNumber += 6) {
+        if (numNumber % iNumber === 0 || numNumber % (iNumber + 2) === 0) return false;
     }
     return true;
 }
 
-var primes = [];
-for (var i = 2; i <= 100; i++) {
-    if (isPrime(i)) {
-        primes.push(i);
+var primesArray = [];
+for (var indexNumber = 2; indexNumber <= 100; indexNumber++) {
+    if (isPrimeBoolean(indexNumber)) {
+        primesArray.push(indexNumber);
     }
 }
 
-console.log("Прості числа від 1 до 100: " + primes.join(", "));
+console.log("Прості числа від 1 до 100: " + primesArray.join(", "));
 
-function fibonacci(n) {
-    var sequence = [0, 1];
-    for (var i = 2; i < n; i++) {
-        sequence[i] = sequence[i - 1] + sequence[i - 2];
+function fibonacciArrayNumber(nNumber) {
+    var sequenceArrayNumber = [0, 1];
+    for (var iNumber = 2; iNumber < nNumber; iNumber++) {
+        sequenceArrayNumber[iNumber] = sequenceArrayNumber[iNumber - 1] + sequenceArrayNumber[iNumber - 2];
     }
-    return sequence;
+    return sequenceArrayNumber;
 }
 
-var fibSequence = fibonacci(20);
+var fibSequenceArrayNumber = fibonacciArrayNumber(20);
 
-console.log("Перші 20 чисел Фібоначчі: " + fibSequence.join(", "));
+console.log("Перші 20 чисел Фібоначчі: " + fibSequenceArrayNumber.join(", "));
 
-function reverseString(str) {
-    return str.split("").reverse().join("");
+function reverseStringFunction(strString) {
+    return strString.split("").reverse().join("");
 }
 
 var originalString = "Привіт, Світ!";
-var reversedString = reverseString(originalString);
+var reversedString = reverseStringFunction(originalString);
 
 console.log("Оригінальний рядок: " + originalString);
 console.log("Перевернутий рядок: " + reversedString);
 
-function countVowels(str) {
-    var count = 0;
-    var vowels = "аеєиіїоуюяАЕЄИІЇОУЮЯ";
-    for (var i = 0; i < str.length; i++) {
-        if (vowels.indexOf(str[i]) !== -1) {
-            count++;
+function countVowelsNumber(strString) {
+    var countNumber = 0;
+    var vowelsString = "аеєиіїоуюяАЕЄИІЇОУЮЯ";
+    for (var indexNumber = 0; indexNumber < strString.length; indexNumber++) {
+        if (vowelsString.indexOf(strString[indexNumber]) !== -1) {
+            countNumber++;
         }
     }
-    return count;
+    return countNumber;
 }
 
-var vowelCount = countVowels(originalString);
+var vowelCountNumber = countVowelsNumber(originalString);
 
-console.log("Кількість голосних у '" + originalString + "': " + vowelCount);
+console.log("Кількість голосних у '" + originalString + "': " + vowelCountNumber);
 
-function removeDuplicates(arr) {
-    var uniqueArr = [];
-    for (var i = 0; i < arr.length; i++) {
-        if (uniqueArr.indexOf(arr[i]) === -1) {
-            uniqueArr.push(arr[i]);
+function removeDuplicatesArray(arrArray) {
+    var uniqueArray = [];
+    for (var indexNumber = 0; indexNumber < arrArray.length; indexNumber++) {
+        if (uniqueArray.indexOf(arrArray[indexNumber]) === -1) {
+            uniqueArray.push(arrArray[indexNumber]);
         }
     }
-    return uniqueArr;
+    return uniqueArray;
 }
 
-var numbersWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
-var uniqueNumbers = removeDuplicates(numbersWithDuplicates);
+var numbersWithDuplicatesArray = [1, 2, 2, 3, 4, 4, 5];
+var uniqueNumbersArray = removeDuplicatesArray(numbersWithDuplicatesArray);
 
-console.log("Унікальні числа: " + uniqueNumbers.join(", "));
+console.log("Унікальні числа: " + uniqueNumbersArray.join(", "));
 
-function isPalindrome(str) {
-    var cleanStr = str.replace(/[^A-Za-zА-Яа-я0-9]/g, '').toLowerCase();
-    return cleanStr === cleanStr.split('').reverse().join('');
+function isPalindromeBoolean(strString) {
+    var cleanStrString = strString.replace(/[^A-Za-zА-Яа-я0-9]/g, '').toLowerCase();
+    return cleanStrString === cleanStrString.split('').reverse().join('');
 }
 
-var palindromeTest = "Я несу гусеня";
-console.log("'" + palindromeTest + "' є паліндромом: " + isPalindrome(palindromeTest));
+var palindromeTestString = "Я несу гусеня";
+console.log("'" + palindromeTestString + "' є паліндромом: " + isPalindromeBoolean(palindromeTestString));
 
-function getCurrentDateTime() {
-    var now = new Date();
-    return now.toString();
+function getCurrentDateTimeString() {
+    var nowDate = new Date();
+    return nowDate.toString();
 }
 
-console.log("Поточна дата та час: " + getCurrentDateTime());
+console.log("Поточна дата та час: " + getCurrentDateTimeString());
 
-function gcd(a, b) {
-    while (b != 0) {
-        var temp = b;
-        b = a % b;
-        a = temp;
+function gcdNumber(aNumber, bNumber) {
+    while (bNumber != 0) {
+        var tempNumber = bNumber;
+        bNumber = aNumber % bNumber;
+        aNumber = tempNumber;
     }
-    return a;
+    return aNumber;
 }
 
-var num1 = 56;
-var num2 = 98;
-var gcdResult = gcd(num1, num2);
+var num1Number = 56;
+var num2Number = 98;
+var gcdResultNumber = gcdNumber(num1Number, num2Number);
 
-console.log("НСД (" + num1 + ", " + num2 + "): " + gcdResult);
+console.log("НСД (" + num1Number + ", " + num2Number + "): " + gcdResultNumber);
 
-function lcm(a, b) {
-    return Math.abs(a * b) / gcd(a, b);
+function lcmNumber(aNumber, bNumber) {
+    return Math.abs(aNumber * bNumber) / gcdNumber(aNumber, bNumber);
 }
 
-var lcmResult = lcm(num1, num2);
+var lcmResultNumber = lcmNumber(num1Number, num2Number);
 
-console.log("НСК (" + num1 + ", " + num2 + "): " + lcmResult);
+console.log("НСК (" + num1Number + ", " + num2Number + "): " + lcmResultNumber);
 
-function generateRandomString(length) {
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var result = '';
-    for (var i = 0; i < length; i++) {
-        result += chars.charAt(generateRandomNumber(0, chars.length - 1));
+function generateRandomString(lengthNumber) {
+    var charsString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var resultString = '';
+    for (var indexNumber = 0; indexNumber < lengthNumber; indexNumber++) {
+        resultString += charsString.charAt(generateRandomNumber(0, charsString.length - 1));
     }
-    return result;
+    return resultString;
 }
 
-var randomStr = generateRandomString(10);
+var randomStrString = generateRandomString(10);
 
-console.log("Випадковий рядок: " + randomStr);
+console.log("Випадковий рядок: " + randomStrString);
 
-var sampleArray = [1, 2, 3, 4, 5];
+var sampleArrayNumber = [1, 2, 3, 4, 5];
 
-var mappedArray = sampleArray.map(function(x) {
-    return x * 2;
+var mappedArrayNumber = sampleArrayNumber.map(function(xNumber) {
+    return xNumber * 2;
 });
 
-console.log("Масив після map: " + mappedArray.join(", "));
+console.log("Масив після map: " + mappedArrayNumber.join(", "));
 
-var filteredArray = sampleArray.filter(function(x) {
-    return x % 2 === 0;
+var filteredArrayNumber = sampleArrayNumber.filter(function(xNumber) {
+    return xNumber % 2 === 0;
 });
 
-console.log("Масив після filter: " + filteredArray.join(", "));
+console.log("Масив після filter: " + filteredArrayNumber.join(", "));
 
-var reducedValue = sampleArray.reduce(function(accumulator, currentValue) {
-    return accumulator + currentValue;
+var reducedValueNumber = sampleArrayNumber.reduce(function(accumulatorNumber, currentValueNumber) {
+    return accumulatorNumber + currentValueNumber;
 }, 0);
 
-console.log("Значення після reduce: " + reducedValue);
+console.log("Значення після reduce: " + reducedValueNumber);
 
-var person = {
-    firstName: "Іван",
-    lastName: "Петренко",
-    age: 30,
-    fullName: function() {
-        return this.firstName + " " + this.lastName;
+var personObject = {
+    firstNameString: "Іван",
+    lastNameString: "Петренко",
+    ageNumber: 30,
+    fullNameFunction: function() {
+        return this.firstNameString + " " + this.lastNameString;
     }
 };
 
-console.log("Повне ім'я: " + person.fullName());
-console.log("Вік: " + person.age);
+console.log("Повне ім'я: " + personObject.fullNameFunction());
+console.log("Вік: " + personObject.ageNumber);
 
-function Rectangle(width, height) {
-    this.width = width;
-    this.height = height;
+function RectangleObject(widthNumber, heightNumber) {
+    this.widthNumber = widthNumber;
+    this.heightNumber = heightNumber;
 }
 
-Rectangle.prototype.area = function() {
-    return this.width * this.height;
+RectangleObject.prototype.areaFunction = function() {
+    return this.widthNumber * this.heightNumber;
 };
 
-var rect = new Rectangle(10, 5);
+var rectObject = new RectangleObject(10, 5);
 
-console.log("Площа прямокутника: " + rect.area());
+console.log("Площа прямокутника: " + rectObject.areaFunction());
