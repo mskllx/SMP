2cb6ed0aadd6af5666c314c544fa6e5f36e30443
Mykhailo Sikaliuk <mishasikalyuk1@gmail.com>
2024-12-08 10:36:07 +0200

    Update contentOperations.ts

--- src/services/contentOperations.ts
+++ src/services/contentOperations.ts
@@ -1,10 +1,10 @@
-import { BaseContent } from '../interfaces/baseContent';
-import {User,Role,Permission} from '../interfaces/user';
+import { BasicContent } from '../interfaces/basicContent';
+import { Account, Role, Access } from '../interfaces/account';
 
-export type ContentOperations<T extends BaseContent> = {
-    create(user:User,item: T): Promise<T>;
-    read(user:User,id: string): Promise<T | null>;
-    update(user:User,id: string, updates: Partial<T>): Promise<T>;
-    delete(user:User,id: string): Promise<void>;
-    list(user:User,params?: any): Promise<T[]>;
+export type ContentManagerActions<T extends BasicContent> = {
+    add(account: Account, item: T): Promise<T>;
+    fetch(account: Account, id: string): Promise<T | null>;
+    modify(account: Account, id: string, changes: Partial<T>): Promise<T>;
+    remove(account: Account, id: string): Promise<void>;
+    listAll(account: Account, filters?: any): Promise<T[]>;
 };
